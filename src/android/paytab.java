70dde36e5f5129c88b708641f69411209a5a400a
Uxman <ZAKI_US94@HOTMAIL.COM>
2018-09-18 16:29:55 +0500

    fixes

--- src/android/paytab.java
+++ src/android/paytab.java
@@ -13,61 +13,63 @@ import org.json.JSONObject;
 public class paytab extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("add")) {
-
-            this.add(args,callbackContext)
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException
+     {
+        if(action.equals("add"))
+        {
+            this.add(args, callbackContext);
             return true;
-        }
-        
-        else if (action.equals("substract")) {
-
-            this.substract(args,callbackContext)
+        }else if(action.equals("substract"))
+        {
+            this.substract(args, callbackContext);
             return true;
         }
         return false;
     }
 
-       private void add(JSONArray args,CallbackContext callback) {
-           if(arg != null){
-               try{
+   
 
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"))
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"))
-
-                callback.success(""+(p1+p2) );
-
-               }catch(Exception ex) {
-                   callback.error('Something went wrong', ex)
-
-               }
-
-           }
-           else {
-               callback.error('Please donot pass null values')
-           }
-      
+    private void add(JSONArray args, CallbackContext callback)
+    {
+        if(args != null)
+        {
+                try
+                {
+                     int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                     int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                       
+                     callback.success(""+ (p1+p2) );
+                          
+                }catch(Exception ex)
+                {
+                    callback.error("Something went wrong "  + ex);
+                }
+        }else
+        {
+            callback.error("Please donot pass null value");
+        }
     }
 
 
-           private void substract(JSONArray args,CallbackContext callback) {
-           if(arg != null){
-               try{
-
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"))
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"))
-
-                callback.success(""+(p1-p2) );
-
-               }catch(Exception ex) {
-                   callback.error('Something went wrong', ex)
-
-               }
-
-           }
-           else {
-               callback.error('Please donot pass null values')
-           }
-      
+    private void substract(JSONArray args, CallbackContext callback)
+    {
+        if(args != null)
+        {
+                try
+                {
+                     int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                     int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                       
+                     callback.success(""+ (p1-p2) );
+                          
+                }catch(Exception ex)
+                {
+                    callback.error("Something went wrong "  + ex);
+                }
+        }else
+        {
+            callback.error("Please donot pass null value");
+        }
     }
+
 }

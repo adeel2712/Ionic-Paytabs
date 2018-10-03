aa11662f1d07d92d67478faa022c7463cca9d2ef
Kunwar  Adeel <kunwar.adeel@gmail.com>
2018-10-03 17:59:16 +0500

    updates of ios

--- src/ios/paytab.swift
+++ src/ios/paytab.swift
@@ -1,69 +1,19 @@
-/********* paytab.m Cordova Plugin Implementation *******/
-
-#import <Cordova/CDV.h>
-
-@interface paytab : CDVPlugin {
-  // Member variables go here.
+@objc(paytab) class paytab : CDVPlugin {
+    func add(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        
+//        NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"]
+//        NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"]
+        
+        let msg = command.arguments[0] as? String ?? ""
+        
+        
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
 }
-
-- (void)add:(CDVInvokedUrlCommand*)command;
-- (void)substract:(CDVInvokedUrlCommand*)command;
-
-
-@end
-
-
-@implementation paytab
-
-- (void)add:(CDVInvokedUrlCommand*)command
-{
-    // CDVPluginResult* pluginResult = nil;
-    // NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"]
-    // NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"]
-
-    // if(param1 >= && param2 >= 0){
-
-    //     NSString* total = @(param1 +param2)
-    //  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
-
-
-    // }
-    // else {
-    //  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-
-    // }
-    // [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-
-}
-
-
-- (void)substract:(CDVInvokedUrlCommand*)command
-{
-    // CDVPluginResult* pluginResult = nil;
-    // NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"]
-    // NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"]
-
-    // if(param1 >= && param2 >= 0){
-
-    //     NSString* total = @(param1 -param2)
-    //  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
-
-
-    // }
-    // else {
-    //  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-
-    // }
-    // [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-
-
-}
-
-
-
-
-
-@
-
-@end
